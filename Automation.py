d30c53415e06bd3dfb563c74192de621c28cd03b
Andy Wu <bwu2sfu@gmail.com>
2017-12-07 17:44:41 -0800

    Implemented a function to add watermarks

--- Automation.py
+++ Automation.py
@@ -1,9 +1,15 @@
 import os
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import datetime
+from datetime import timedelta
+
+
+from selenium.webdriver.common.action_chains import ActionChains
 
 from Helpers import Helpers
 from Variables import VariableClass as V
@@ -33,36 +39,84 @@ class Automations:
             pass
 
         # define watermark name
-        wmNameElem = Automations.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "input[id$='Name']")))
+        wmNameElem = Automations.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, V.watermarkName)))
         wmNameElem[0].send_keys(name)
 
         # define watearmark text
-        wmTextElem = Automations.wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='e.g. Unlocked by John Smith']")))
+        wmTextElem = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.watermarkText)))
         wmTextElem.send_keys(wmText)
 
         # Save and Exit
-        saveBtn = Automations.wait.until(EC.presence_of_element_located((By.XPATH, "//button[@data-action='save']")))
+        saveBtn = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.saveBtn)))
         saveBtn.click()
 
+    def addPolicy(self, policyName, expiryDate, expiryAfterUnlock, offlineAccess, OpenLimit, totalLimit, pdfLimit, browserLimit, webPrint, downloadPrint, accountLimit, ipLimit):
+
+        Automations.driver.get(V.drmPolicyUrl)
+
+        # 'ADD DRM POLICY' button
+        RunAutomation.wait.until(EC.presence_of_element_located((By.XPATH, V.addDRMBtn))).click()
+
+        policyNameElem = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.policyNameField)))
+        policyNameElem.send_keys(policyName)
+
+        time.sleep(1)
+
+        # Expiry Date
+        if expiryDate == 'Never':
+            ExpiryDateElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'ExpiryF')]")))
+            ExpiryDateElem[1].click()
+        elif expiryDate != 'Not Set':
+            ExpiryDateElem = Automations.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'date-picker-group')))
+            ActionChains(Automations.driver).move_to_element(ExpiryDateElem[0]).click().send_keys(expiryDate).perform()
+
+
+
+
+
+        # Expiry After First Unlock
+
+
+        # Offline Access
+
+
+        # Content Open Limit
+
+
+
+
 if __name__ == "__main__":
+
+    tomorrow = datetime.datetime.strptime(str(datetime.date.today() + timedelta(days=1)), '%Y-%m-%d').strftime('%m/%d/%Y')
+    print(tomorrow)
+
     RunAutomation = Automations()
 
 
+
+    # Create a DRM policy
+    RunAutomation.addPolicy('Unlimited', tomorrow, 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test')
+
+
     # Create User Spcific watermarks
-    RunAutomation.addWatermarks('us', 'Test User ID', '_userId_')
-    RunAutomation.addWatermarks('us', 'Test User Name', '_userName_')
-    RunAutomation.addWatermarks('us', 'Test Date', '_date_')
-    RunAutomation.addWatermarks('us', 'Test Time', '_time_')
-    RunAutomation.addWatermarks('us', 'Test Document Code', '_docCode_')
-    RunAutomation.addWatermarks('us', 'Test Computer ID', '_computerId_')
-    RunAutomation.addWatermarks('us', 'Test New Line', '_newLine_')
-    RunAutomation.addWatermarks('us', 'Test User Custom Field', '_readerCustomField_')
-    RunAutomation.addWatermarks('us', 'Test User External Key', '_readerExternalKey_')
-    RunAutomation.addWatermarks('us', 'Test Unique Open ID', '_unique_open_id_')
+    # RunAutomation.addWatermarks('us', 'Test User ID', '_userId_')
+    # RunAutomation.addWatermarks('us', 'Test User Name', '_userName_')
+    # RunAutomation.addWatermarks('us', 'Test Date', '_date_')
+    # RunAutomation.addWatermarks('us', 'Test Time', '_time_')
+    # RunAutomation.addWatermarks('us', 'Test Document Code', '_docCode_')
+    # RunAutomation.addWatermarks('us', 'Test Computer ID', '_computerId_')
+    # RunAutomation.addWatermarks('us', 'Test New Line', '_newLine_')
+    # RunAutomation.addWatermarks('us', 'Test User Custom Field', '_readerCustomField_')
+    # RunAutomation.addWatermarks('us', 'Test User External Key', '_readerExternalKey_')
+    # RunAutomation.addWatermarks('us', 'Test Unique Open ID', '_unique_open_id_')
 
     # Create Text Only Watermark
 
 
 
-    # Create an unlimited DRM policy
+
+
+
+
+
     # RunAutomation.addDrmPolicy("Unlimited")
