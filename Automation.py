dfa4d5a455df87d4b58375920f241ad300aa6508
Andy Wu <bwu2sfu@gmail.com>
2018-03-07 14:05:48 -0800

    Implemented a function to add watermarks

--- Automation.py
+++ Automation.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import datetime
 from datetime import timedelta
-
+from selenium.webdriver.support.ui import Select
 
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -21,14 +21,15 @@ class Automations:
     driver = webdriver.Chrome(chromedriver)
 
     h = Helpers()
-    h.userLogin(driver)
+    h.userLogin(driver, 'user')
     driver.maximize_window()
     wait = WebDriverWait(driver, 20)
 
     def __init__(self):
         pass
 
-    def addWatermarks(self, wmType, name, wmText):
+    # ADD WATERMARKS
+    def addWatermarks(self, wmType, name, wmText, placement):
 
         Automations.driver.get(V.watermarkUrl)
 
@@ -36,19 +37,47 @@ class Automations:
         Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.addWmBtn))).click()
 
         if wmType == 'to':
-            pass
+            select = Select(Automations.wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "select[id$='SetContentAt']"))))
+            select.select_by_value("DocumentProcessingTime")
 
-        # define watermark name
-        wmNameElem = Automations.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, V.watermarkName)))
-        wmNameElem[0].send_keys(name)
+            # define watermark name
+            wmNameElem = Automations.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, V.watermarkName)))
+            wmNameElem[0].send_keys(name)
 
-        # define watearmark text
-        wmTextElem = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.watermarkText)))
-        wmTextElem.send_keys(wmText)
+            # define watermark text
+            wmTextElem = Automations.wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='e.g. CONFIDENTIAL']")))
+            wmTextElem.send_keys(wmText)
 
-        # Save and Exit
-        saveBtn = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.saveBtn)))
-        saveBtn.click()
+            # define placement
+            if placement == 'up':
+                upPosition = Automations.wait.until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "[data-id='up']")))
+                upPosition.click()
+            elif placement == 'down':
+                downPosition = Automations.wait.until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "[data-id='down']")))
+                downPosition.click()
+
+            # Save and Exit
+            time.sleep(2)
+            saveBtn = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.saveBtn)))
+            saveBtn.click()
+
+
+        elif wmType == 'us':
+            # define watermark name
+            wmNameElem = Automations.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, V.watermarkName)))
+            wmNameElem[0].send_keys(name)
+
+            # define watermark text
+            wmTextElem = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.watermarkText)))
+            wmTextElem.send_keys(wmText)
+
+            # Save and Exit
+            time.sleep(2)
+            saveBtn = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.saveBtn)))
+            saveBtn.click()
 
     def addPolicy(self, policyName, expiryDate, expiryAfterUnlock, offlineAccess, OpenLimit, totalLimit, pdfLimit, browserLimit, webPrint, downloadPrint, accountLimit, ipLimit):
 
@@ -62,6 +91,9 @@ class Automations:
 
         time.sleep(1)
 
+
+        ################### EXPIRATION CONTROL ###################
+
         # Expiry Date
         if expiryDate == 'Never':
             ExpiryDateElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'ExpiryF')]")))
@@ -71,63 +103,258 @@ class Automations:
             ActionChains(Automations.driver).move_to_element(ExpiryDateElem[0]).click().send_keys(expiryDate).perform()
 
         # Expiry After First Unlock
-        # if expiryDate == 'Never':
-        #     ExpiryDateElem = Automations.wait.until(
-        #         EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'ExpiryF')]")))
-        #     ExpiryDateElem[1].click()
-        # elif expiryDate != 'Not Set':
-        #     ExpiryDateElem = Automations.wait.until(
-        #         EC.presence_of_all_elements_located((By.CLASS_NAME, 'date-picker-group')))
-        #     ActionChains(Automations.driver).move_to_element(ExpiryDateElem[0]).click().send_keys(
-        #         expiryDate).perform()
+        ExpiryUnlockElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'RelativeExpiryInDaysF')]")))
+        if expiryAfterUnlock == 'Never':
+            ExpiryUnlockElem[1].click()
+        elif expiryAfterUnlock != 'Not Set':
+            ActionChains(Automations.driver).move_to_element(ExpiryUnlockElem[2]).click().send_keys(expiryAfterUnlock).perform()
 
 
-
-
-        # Expiry After First Unlock
-
+        ################### ACCESS CONTROL ###################
 
         # Offline Access
-
+        offineAccessElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'OfflineDurationinDaysF')]")))
+        if offlineAccess == 'Never':
+            offineAccessElem[1].click()
+        elif offlineAccess != 'Not Set':
+            ActionChains(Automations.driver).move_to_element(offineAccessElem[2]).click().send_keys(offlineAccess).perform()
 
         # Content Open Limit
+        openLimitElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'OpenLimitF')]")))
+        if offlineAccess == 'Never':
+            openLimitElem[1].click()
+        elif offlineAccess != 'Not Set':
+            ActionChains(Automations.driver).move_to_element(openLimitElem[2]).click().send_keys(OpenLimit).perform()
+
+        ################### PDF/BROWSER CONTROL ###################
+
+        totalLimitElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'ComputersMaxF')]")))
+        if totalLimit == 'Never':
+            totalLimitElem[1].click()
+        elif totalLimit != 'Not Set':
+            ActionChains(Automations.driver).move_to_element(totalLimitElem[2]).click().send_keys(totalLimit).perform()
 
 
+        ################### PRINT CONTROL ###################
+
+        # Web Browser Print Limit
+        webPrintLimitElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'WebPrintLimitF')]")))
+        if webPrint == 'Never':
+            webPrintLimitElem[1].click()
+        elif webPrint != 'Not Set':
+            ActionChains(Automations.driver).move_to_element(webPrintLimitElem[2]).click().send_keys(webPrint).perform()
+
+
+        # Download to Print Limit
+        # It took me a while to figure out the following xpath expression!!
+        downloadPrintLimitElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'PrintLimitF') and not(contains(@name, 'WebPrintLimitF'))]")))
+        if downloadPrint == 'Never':
+            downloadPrintLimitElem[1].click()
+        elif downloadPrint != 'Not Set':
+            ActionChains(Automations.driver).move_to_element(downloadPrintLimitElem[2]).click().send_keys(downloadPrint).perform()
+
+
+        ################### OTHER CONTROLS ###################
+
+        # Library or Account Limit
+        accountLimitElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'DocumentLimitF')]")))
+        if accountLimit == 'Never':
+            accountLimitElem[1].click()
+        elif accountLimit != 'Not Set':
+            ActionChains(Automations.driver).move_to_element(accountLimitElem[2]).click().send_keys(accountLimit).perform()
+
+        # IP Address Limit
+        ipLimitElem = Automations.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@name, 'IpAddressesMaxF')]")))
+        if ipLimit == 'Never':
+            ipLimitElem[1].click()
+        elif ipLimit != 'Not Set':
+            ActionChains(Automations.driver).move_to_element(ipLimitElem[2]).click().send_keys(ipLimit).perform()
+
+        time.sleep(2)
+        Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.saveBtn))).click()
+
+
+    def addContentSettings(self, csType, name, allowPrinting, allowCP, selectWatermark, disableAnnotation, casePassword):
+
+        Automations.driver.get(V.contentSettingsUrl)
+
+        # 'ADD SETTINGS' button
+        Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.addCSBtn))).click()
+
+        # define name of Content Settings
+        csNameElem = Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id$='Name']")))
+        csNameElem.send_keys(name)
+
+        # Allow Printing
+        if allowPrinting == 'true':
+            Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id$='AllowPrint']"))).click()
+
+        # Allow copy and paste
+        if allowCP == 'true':
+            Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id$='AllowCopy']"))).click()
+
+
+        # Select Login Form
+        selectLoginForm = Select(Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "select[id$='FormId']"))))
+        selectLoginForm.select_by_visible_text("new-login")
+
+
+        # Select first watermark
+        selectWatermark = Select(Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "select[id^='MultipleSelectionForm']"))))
+        selectWatermark.select_by_visible_text('Test Computer ID')
+
+        # TODO: select more watermarks
+
+        # go to the next Tab
+        Automations.wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Advanced Options"))).click()
+
+        # disable annotation
+        if disableAnnotation == 'true':
+            Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id$='DisableAnnotations']"))).click()
+
+        # if using a non-Case Sensitive password
+        if casePassword == 'false':
+            Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id$='CaseSensitivePassword']"))).click()
+
+        time.sleep(2)
+        saveBtn = Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.saveBtn)))
+        saveBtn.click()
+
+    # ADD SINGLE USERS
+    def addSingleUsers(self, name, password, notes, externalKey, customField):
+
+        Automations.driver.get(V.usersUrl)
+        Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.addSingleUserBtn))).click()
+
+        uNameElem = Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.uName)))
+        uNameElem.send_keys(name)
+
+        uPasswordElem = Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.uPassword)))
+        uPasswordElem.send_keys(password)
+
+        uNotesElem = Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.uNotes)))
+        uNotesElem.send_keys(notes)
+
+        uKeyElem = Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.uKey)))
+        uKeyElem.send_keys(externalKey)
+
+        uFieldElem = Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.uField)))
+        uFieldElem.send_keys(customField)
+
+        time.sleep(2)
+        Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.saveBtn))).click()
+
+    # ADD MULTIPLE USERS
+    def addMultipleUsers(self, name0, password0, name1, password1, name2, password2, name3, password3, name4, password4, name5, password5):
+
+        Automations.driver.get(V.usersUrl)
+        Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.addMultipleUserBtn))).click()
+
+        time.sleep(1)
+        # Strategy: make sure the 'ADD ANOTHER' button is functional
+        Automations.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, V.addOneMoreUserBtn))).click()
+
+        uNameElem = Automations.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, V.uName)))
+        uPasswordElem = Automations.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, V.mPassword)))
+
+        # USER 0
+        uNameElem[0].send_keys(name0)
+        uPasswordElem[0].send_keys(password0)
+
+        # USER 1
+        uNameElem[1].send_keys(name1)
+        uPasswordElem[1].send_keys(password1)
+
+        # USER 2
+        uNameElem[2].send_keys(name2)
+        uPasswordElem[2].send_keys(password2)
+
+        # USER 3
+        uNameElem[3].send_keys(name3)
+        uPasswordElem[3].send_keys(password3)
+
+        # USER 4
+        uNameElem[4].send_keys(name4)
+        uPasswordElem[4].send_keys(password4)
+
+        # USER 5
+        uNameElem[5].send_keys(name5)
+        uPasswordElem[5].send_keys(password5)
+
+        time.sleep(2)
+        Automations.wait.until(EC.presence_of_element_located((By.XPATH, V.saveBtn))).click()
 
 
 if __name__ == "__main__":
 
     # Tomorrow
     tomorrow = datetime.datetime.strptime(str(datetime.date.today() + timedelta(days=1)), '%Y-%m-%d').strftime('%m/%d/%Y')
-    print(tomorrow)
+    print('The date of tomorrow is: ', tomorrow)
 
     RunAutomation = Automations()
 
-
-
     # Create a DRM policy
-    RunAutomation.addPolicy('Unlimited', tomorrow, 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test')
+    # addPolicy(self, policyName, expiryDate, expiryAfterUnlock, offlineAccess, OpenLimit, totalLimit, pdfLimit, browserLimit, webPrint, downloadPrint, accountLimit, ipLimit)
+
+    # DRM Policy 1 - Unlimited
+    # RunAutomation.addPolicy('Unlimited', 'Never', 'Never', 'Never', 'Never', 'Never', 'NA', 'NA', 'Never', 'Never', 'Never', 'Never')
+
+    # DRM Policy 2 - Web Print : 2; Download Print : 3
+    # RunAutomation.addPolicy('Print - Web2 - Download3', 'Never', 'Never', 'Never', 'Never', 'Never', 'NA', 'NA', '2', '3', 'Never', 'Never')
+
+
+    # DRM Policy - No Offline Access
+    # RunAutomation.addPolicy('No Offline Access', 'Never', 'Never', 'Not Set', 'Never', 'Never', 'NA', 'NA', '2', '3', 'Never', 'Never')
+
+    # DRM Policy 3 - PDF_1 - Web_1
+    # RunAutomation.addPolicy('PDF_1 - Web_1', 'Never', 'Never', 'Never', 'Never', 'NA', '1', '1', '2','3', 'Never', 'Never')
+
+    # DRM Policy 4 -
+
+    # DRM Policy 5 -
 
 
     # Create User Spcific watermarks
-    # RunAutomation.addWatermarks('us', 'Test User ID', '_userId_')
-    # RunAutomation.addWatermarks('us', 'Test User Name', '_userName_')
-    # RunAutomation.addWatermarks('us', 'Test Date', '_date_')
-    # RunAutomation.addWatermarks('us', 'Test Time', '_time_')
-    # RunAutomation.addWatermarks('us', 'Test Document Code', '_docCode_')
-    # RunAutomation.addWatermarks('us', 'Test Computer ID', '_computerId_')
-    # RunAutomation.addWatermarks('us', 'Test New Line', '_newLine_')
-    # RunAutomation.addWatermarks('us', 'Test User Custom Field', '_readerCustomField_')
-    # RunAutomation.addWatermarks('us', 'Test User External Key', '_readerExternalKey_')
-    # RunAutomation.addWatermarks('us', 'Test Unique Open ID', '_unique_open_id_')
 
-    # Create Text Only Watermark
+    # RunAutomation.addWatermarks('us', 'Test User ID', '_userId_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test User Name', '_userName_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test Date', '_date_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test Time', '_time_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test Document Code', '_docCode_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test Computer ID', '_computerId_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test New Line', '_newLine_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test User Custom Field', '_readerCustomField_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test User External Key', '_readerExternalKey_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test Unique Open ID', '_unique_open_id_', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test DELETE watermark', 'THIS WATERMARK WILL BE DELETED!', 'bc')
+    # RunAutomation.addWatermarks('us', 'Test DELETE watermark2', 'THIS WATERMARK WILL BE DELETED!', 'bc')
+    #
+    # # Create Text Only Watermark
+    # RunAutomation.addWatermarks('to', 'Test Text Only Watermark - CENTER', 'HELLO PYTHON - center', 'center')
+    # RunAutomation.addWatermarks('to', 'Test Text Only Watermark - UP', 'HELLO PYTHON - up', 'up')
+    # RunAutomation.addWatermarks('to', 'Test Text Only Watermark - DOWN', 'HELLO PYTHON - down', 'down')
 
 
+    RunAutomation.addSingleUsers('cs', 'cs', 'Created this user to test case sensitive password feature', '$ValidExternalKey$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('Andy', 'Test123', 'Im the KING!!', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('Ben', 'Test123', 'MBA Classmate', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('Cindy', 'Test123', 'Scream Movie', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('Daisy', 'Test123', 'Botanic Garden', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('Eli', 'Test123', 'There will be blood!', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('Frank', 'Test123', 'Everybody Loves Raymond!', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('Grace', 'Test123', 'Saving Grace!', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('Hellen', 'Test123', 'Godness', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('To Be Deleted Later 1', 'Test123', 'Saving Grace!', '$Brain$', 'This is the info located in the custom field')
+    RunAutomation.addSingleUsers('To Be Deleted Later 2', 'Test123', 'Godness', '$Brain$', 'This is the info located in the custom field')
 
+    # Add Multiple Users
+    RunAutomation.addMultipleUsers('mAndy', 'Test123', 'mBen', 'Test123', 'mCindy', 'Test123', 'mDaisy', 'Test123', 'mEli', 'Test123', 'mFrank', 'Test123')
 
+    # Create Content Setting
+    # (csType, name, allowPrinting, allowCP, selectWatermark, disableAnnotation, casePassword)
+    # RunAutomation.addContentSettings('full', 'Full', 'true', 'true', 'Test Computer ID', 'false', 'true')
 
+    # Add Single User
+    # def addSingleUsers(self, name, password, notes, externalKey, customField):
 
-
-
-    # RunAutomation.addDrmPolicy("Unlimited")
+    # Add GROUPS
